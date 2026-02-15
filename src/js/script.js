22f93721a0a96bc19c4642c063b1bc501c3764ae
Heitor Queiroz <heitorqueirozes@gmail.com>
2026-02-14 21:26:29 -0300

    home e responsividade mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars');
+        $(this).find('i').toggleClass('fa-xmark');
     });
 });
